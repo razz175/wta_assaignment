e59180227dba1b718c7285a66d0401202b7dff8d
razz175 <50497431+razz175@users.noreply.github.com>
2020-06-18 00:03:26 +0530

    Delete appmodule.ts

--- AngularApp2/src/app/appmodule.ts
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/mydb', (err) => {
-    if (err) throw err;
-    console.log("connected");
-});
-
-module.exports = mongoose;
